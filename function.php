9628396412309c2798ded0b8fb5019437882ace6
YaKem <ykemouche@gmail.com>
2020-08-01 11:26:15 +0200

    Add icons to clean, modify and remove item, add functions for add same product and modify all item, change css style for all the page

--- function.php
+++ function.php
@@ -9,33 +9,79 @@ function createBasket() {
 
 function addItem($name, $quantity, $price, $id = null) {
     if(createBasket()) {
-        if(array_key_exists($id, $_SESSION['basket'])) {
-            $_SESSION['basket'][$id]['quantity']++;
+        if($id == null) {
+            $res = false;
+            for($i = 0; $i < count($_SESSION['basket']); $i++) {
+                if($_SESSION['basket'][$i]['name'] == $name && $_SESSION['basket'][$i]['price'] == $price) {
+                    $_SESSION['basket'][$i]['quantity'] += $quantity;
+                    $res = true;
+                }
+            }
+            if($res != true) {
+                $_SESSION['basket'][] = [
+                    'name'     => $name,
+                    'quantity' => $quantity,
+                    'price'    => $price
+                ];
+            }
         } else {
-            $_SESSION['basket'][] = ['name' => $name, 'quantity' => $quantity, 'price' => $price];
+            if($_SESSION['basket'][$id]['name'] != $name) {
+                $_SESSION['basket'][$id]['name'] = $name;
+            }
+            if($_SESSION['basket'][$id]['quantity'] != $quantity) {
+                $_SESSION['basket'][$id]['quantity'] = $quantity;
+            }
+            if($_SESSION['basket'][$id]['price'] != $price) {
+                $_SESSION['basket'][$id]['price'] = $price;
+            }
         }
     }
 }
 
 function removeItem($id) {
     if(createBasket()) {
-        $length = count($_SESSION['basket']);
-        $tmp = [];
-    
-        for($i = 0; $i < $length; $i++){
-            if($i !== $id) {
-                $tmp[] = $_SESSION['basket'][$i];
-            }
-        }
         
-        $_SESSION['basket'] = $tmp;
+        if(array_key_exists(intval($id), $_SESSION['basket'])) {
+            // var_dump($_SESSION['basket']);
+            $length = count($_SESSION['basket']);
+            
+            if($length == 1) {
     
-        unset($tmp);    
+                $_SESSION['basket'] = [];
+    
+            } else {
+    
+                $tmp = [];
+    
+                for($i = 0; $i < $length; $i++){
+                    echo $i;
+                    if($i != $id) {
+                        $tmp[] = $_SESSION['basket'][$i];
+                    }
+                }
+    
+                $_SESSION['basket'] = $tmp;
+                unset($tmp);    
+            }
+        }   
     }      
 }
 
-function removeBasket($name) {
-    unset($_SESSION[$name]);
+function removeBasket() {
+    unset($_SESSION['basket']);
+}
+
+function getTotal() {
+    $total = 0;
+    for($i = 0; $i < count($_SESSION['basket']); $i++) {
+        $total += $_SESSION['basket'][$i]['quantity'] * $_SESSION['basket'][$i]['price'];
+    }
+
+    return $total;
+}
+
+function setInputVal($name, $id) {
+    $_SESSION['input'][$name] = $_SESSION['basket'][$id][$name];
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,99 +1,109 @@
 <?php
 
-if(session_status() === 'PHP_SESSION_NONE') {
-    session_start();
-}
+session_start();
 
 include 'function.php';
-echo $_SERVER['REQUEST_METHOD'];
-if(!empty($_POST['name']) && !empty($_POST['quantity']) && !empty($_POST['price'])) {
-    addItem($_POST['name'], $_POST['quantity'], $_POST['price']);
+
+if(isset($_SESSION['input'])) {
+    unset($_SESSION['input']);
+    unset($id);
 }
 
+if(!empty($_POST['name']) && !empty($_POST['quantity']) && !empty($_POST['price'])) {
+    addItem($_POST['name'], $_POST['quantity'], $_POST['price'], $_POST['id']);
+}
 
-if(!empty($_GET['action']) && !empty($_GET['id'])) { 
-    if($_GET['action'] === 'delete') {        
+if(!empty($_GET['action'])) {
+    if($_GET['action'] == 'modify' && isset($_GET['id'])) {
+        $id = $_GET['id'];
+
+        setInputVal('name', $id);
+        setInputVal('quantity', $id);
+        setInputVal('price', $id);
+
+    } else if($_GET['action'] == 'delete' && isset($_GET['id'])) {      
         removeItem($_GET['id']);
-    } else if($_GET['action'] === 'empty') {
-        removeBasket('basket');
+    } else if($_GET['action'] == 'empty') {
+        removeBasket();
     }
 }
-
-var_dump($_SESSION['basket']);
 
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
         "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<html lang="en">
 <head>
-    <title></title>
+    <title>My PHP Basket</title>
+    <script src="https://kit.fontawesome.com/ac5628c3f3.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./css/style.css" />
-    <style>
-        ul {
-            list-style-type: none;
-            margin-left: 0;
-        }
-        fieldset {
-            width: 200px;
-            padding: 10px;
-            margin-top: 30px;
-        }
-    </style>
 </head>
 <body>
-    <?php if(!empty($_SESSION['basket'])): ?>
-        <table border=1>
-            <thead>
-                <tr>
-                    <th>Nom</th>
-                    <th>Quantité</th>
-                    <th>Prix</th>
-                    <th>Actions</th>
-                </tr>            
-            </thead>
-            <tbody>        
-                <?php for($i = 0;  $i < count($_SESSION['basket']); $i++): ?>
+    <div class="container">       
+        <?php if(!empty($_SESSION['basket'])): ?>
+            <table>
+                <thead>
                     <tr>
-                        <td><?= $_SESSION['basket'][$i]['name'] ?></td>
-                        <td><?= $_SESSION['basket'][$i]['quantity'] ?></td>
-                        <td><?= $_SESSION['basket'][$i]['price'] ?></td>
-                        <td><a href="index.php?action=delete&id=<?= $i ?>">Delete</a></td>
+                        <th>Nom</th>
+                        <th>Quantité</th>
+                        <th>Prix</th>
+                        <th>Total</th>
+                        <th>Actions</th>
+                    </tr>            
+                </thead>
+                <tbody>        
+                    <?php for($i = 0;  $i < count($_SESSION['basket']); $i++): ?>
+                        <tr>
+                            <td><?= $_SESSION['basket'][$i]['name'] ?></td>
+                            <td><?= $_SESSION['basket'][$i]['quantity'] ?></td>
+                            <td><?= $_SESSION['basket'][$i]['price'] ?></td>
+                            <td><?= $_SESSION['basket'][$i]['quantity'] * $_SESSION['basket'][$i]['price'] ?>€</td>
+                            <td>
+                                <a href="index.php?action=modify&id=<?= $i ?>"><i class="fa fa-pen"></i></a>
+                                <a href="index.php?action=delete&id=<?= $i ?>"><i class="fa fa-trash"></i></a>
+                            </td>
+                        </tr>
+                    <?php endfor ?>
+                </tbody> 
+                <tfoot>
+                    <tr>
+                        <td>
+                            <a href="index.php?action=empty"><i class="fa fa-recycle"></i></a>
+                        </td>
+                        <td colspan="4">
+                            Total: <?= getTotal() ?>€
+                        </td>
                     </tr>
-                <?php endfor ?>
-            </tbody> 
-            <tfoot>
-                <tr>
-                    <td colspan="4">
-                        <a href="index.php?action=empty">Vider</a>
-                    </td>
-                </tr>
-            </tfoot>  
-        </table>
-    <?php else: ?>
-        <p>Basket vide</p>
-    <?php endif ?>
-    <form action="index.php" method="post">
-        <fieldset>
-            <ul>
-                <li>
-                    <label for="name">Nom: </label>
-                    <input id="name" type="text" name="name" />
-                </li>
-                <li>
-                    <label for="qty">Quantité: </label>
-                    <input id="qty" type="text" name="quantity" />
-                </li>
-                <li>
-                    <label for="prx">Prix: </label>
-                    <input id="prx" type="text" name="price" />
-                </li>
-                <li>
-                    <input type="submit" value="Ajouter" />
-                </li>
-            </ul>
-        </fieldset>    
-    </form>
+                </tfoot>  
+            </table>
+        <?php else: ?>
+            <div id="msg">
+                <p>Basket vide</p>
+            </div>
+        <?php endif ?>
+        <form action="index.php" method="post">
+            <fieldset>
+                <ul>
+                    <input type="hidden" name="id" value="<?= $id ?? '' ?>">
+                    <li>
+                        <label for="name">Nom: </label>
+                        <input id="name" type="text" name="name" value="<?= $_SESSION['input']['name'] ?? '' ?>" />
+                    </li>
+                    <li>
+                        <label for="qty">Quantité: </label>
+                        <input id="qty" type="text" name="quantity" value="<?= $_SESSION['input']['quantity'] ?? '' ?>" />
+                    </li>
+                    <li>
+                        <label for="prx">Prix: </label>
+                        <input id="prx" type="text" name="price" value="<?= $_SESSION['input']['price'] ?? '' ?>" />
+                    </li>
+                    <li>
+                        <input type="submit" value="Ajouter" />
+                    </li>
+                </ul>
+            </fieldset>    
+        </form>
+    </div>
 </body>
 </html>
 
